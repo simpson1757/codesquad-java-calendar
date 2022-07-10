8d37aaa49cd600e07bd28874c603b49c31370008
user <user@DESKTOP-31T5EEV>
2022-07-10 10:18:38 +0900

    Fix encoding problem UTF-8 적용

--- src/calendar_project/Calendar.java
+++ src/calendar_project/Calendar.java
@@ -3,7 +3,19 @@ package calendar_project;
 public class Calendar {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello, Calendar!");
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("----------------");
+
+		for (int i = 0; i <= 21; i += 7) {
+			for (int j = 1; j <= 7; j++) {
+				System.out.print(i + j + " ");
+
+			}
+			System.out.println();
+
+		}
 	}
 
 }
